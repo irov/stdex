2dd987737318edd9e88c52d0ba10a0b8c05ee71f
yuriy.levchenko <irov13@mail.ru>
2019-06-11 21:02:16 +0300

    improve debug intrusive_ptr

--- include/stdex/intrusive_ptr.h
+++ include/stdex/intrusive_ptr.h
@@ -19,10 +19,30 @@
 #endif
 
 #ifdef STDEX_INTRUSIVE_PTR_DEBUG
-#   define STDEX_INTRUSIVE_PTR_CHECK_TYPECAST_PTR(PTR, TYPE) if( PTR != nullptr && dynamic_cast<TYPE>(PTR) == nullptr ) { STDEX_CRITICAL_CRASH_ERROR; STDEX_THROW_EXCEPTION("ptr invalid cast");}
-#	define STDEX_INTRUSIVE_PTR_DECLARE_DEBUG_MASK() uint32_t m_debug_ptr_mask__;
+template<class V, class T>
+inline void stdex_intrusive_ptr_check_typecast_ptr( T _ptr )
+{
+    if( _ptr != nullptr && dynamic_cast<V>(_ptr) == nullptr )
+    { 
+        STDEX_CRITICAL_CRASH_ERROR; 
+        STDEX_THROW_EXCEPTION( "ptr invalid cast" ); 
+    }
+}
+
+template<class T>
+inline void stdex_intrusive_ptr_check_debug_mask( T _ptr )
+{
+    if( _ptr->m_debug_ptr_mask__ != 0xABCDEF01 ) 
+    { 
+        STDEX_CRITICAL_CRASH_ERROR;
+        STDEX_THROW_EXCEPTION( "mask != 0xABCDEF01" ); 
+    }
+}
+
+#   define STDEX_INTRUSIVE_PTR_CHECK_TYPECAST_PTR(PTR, TYPE) stdex_intrusive_ptr_check_typecast_ptr<TYPE>( PTR )
+#	define STDEX_INTRUSIVE_PTR_DECLARE_DEBUG_MASK() public: uint32_t m_debug_ptr_mask__; protected:
 #	define STDEX_INTRUSIVE_PTR_INIT_DEBUG_MASK() this->m_debug_ptr_mask__ = 0xABCDEF01
-#	define STDEX_INTRUSIVE_PTR_CHECK_DEBUG_MASK() if( this->m_debug_ptr_mask__ != 0xABCDEF01 ) { STDEX_CRITICAL_CRASH_ERROR; STDEX_THROW_EXCEPTION("mask != 0xABCDEF01");}
+#	define STDEX_INTRUSIVE_PTR_CHECK_DEBUG_MASK() stdex_intrusive_ptr_check_debug_mask( this )
 #else
 #   define STDEX_INTRUSIVE_PTR_CHECK_TYPECAST_PTR(PTR, TYPE)
 #	define STDEX_INTRUSIVE_PTR_DECLARE_DEBUG_MASK()
