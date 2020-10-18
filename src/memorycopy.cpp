310695b3832451f53920539bf54400a2bbe78f42
yuriy.levchenko <irov13@mail.ru>
2020-10-18 22:21:30 +0300

    improve memorycopy

--- src/memorycopy.cpp
+++ src/memorycopy.cpp
@@ -2,6 +2,8 @@
 
 #include <algorithm>
 
+#include <stdint.h>
+
 //#include "stdex/unchecked_array_iterator.h"
 
 namespace stdex
@@ -12,7 +14,7 @@ namespace stdex
         void memorycopy_impl( void * _dist, size_t _offset, const void * _src, size_t _size )
         {
             //std::copy( (unsigned char *)_src, (unsigned char *)_src + _size, stdex::unchecked_array_iterator<unsigned char *>((unsigned char *)_dist + _offset) );
-            std::copy( (unsigned char *)_src, (unsigned char *)_src + _size, (unsigned char *)_dist + _offset );
+            std::copy( (uint8_t *)_src, (uint8_t *)_src + _size, (uint8_t *)_dist + _offset );
         }
     }
 }
